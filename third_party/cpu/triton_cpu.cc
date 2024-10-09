f9f1a7d5ae9d9284f04621cf681121494805b326
Adam Siemieniuk <adam.siemieniuk@intel.com>
2024-10-09 14:27:25 +0200

    Triton to XSMM
    Adds lowering pass from triton to XSMM microkernels.
    XSMM utility APIs are generalized to work on opaque operations
    representing contractions.
    
    A simple pattern mapping tt.dot to XSMM kernel is added.
    The runtime lowering to XSMM is now controlled by two separate flags:
    - TRITON_CPU_VECTOR_XSMM=1 to lower from vector as before
    - TRITON_CPU_TRITON_XSMM=1 to lower from triton ops

--- third_party/cpu/triton_cpu.cc
+++ third_party/cpu/triton_cpu.cc
@@ -9,6 +9,7 @@
 #include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
 #include "mlir/Conversion/Passes.h"
 #include "mlir/Conversion/VectorToLLVM/ConvertVectorToLLVMPass.h"
+#include "mlir/Dialect/Func/Extensions/AllExtensions.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
 #include "mlir/Dialect/Vector/Transforms/Passes.h"
 #include "mlir/Pass/Pass.h"
@@ -144,6 +145,9 @@ void init_triton_cpu_passes_ttcpuir(py::module &&m) {
   m.def("add_convert_vector_to_xsmm", [](mlir::PassManager &pm) {
     pm.addPass(mlir::triton::cpu::createConvertVectorToXsmm());
   });
+  m.def("add_convert_triton_to_xsmm", [](mlir::PassManager &pm) {
+    pm.addPass(mlir::triton::cpu::createConvertTritonToXsmm());
+  });
 }
 
 void init_triton_cpu(py::module &&m) {
@@ -155,6 +159,7 @@ void init_triton_cpu(py::module &&m) {
     registry.insert<mlir::triton::cpu::TritonCPUDialect,
                     mlir::vector::VectorDialect>();
     mlir::triton::cpu::registerTritonOpScalarizeExternalModels(registry);
+    mlir::func::registerAllExtensions(registry);
     context.appendDialectRegistry(registry);
     context.loadAllAvailableDialects();
   });
